94ab5812f0db8353242e1058f0ee3b8c1730cd65
mostofa kamal <mostofakamal1990@gmail.com>
2022-05-05 22:24:56 +0600

    backend initalazie

---
+++ index.js
@@ -0,0 +1,140 @@
+const express = require('express');
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const objectId = require('mongodb').ObjectId;
+const app = express();
+
+// port number 
+const port = process.env.PORT || 5000;
+
+// Middleware 
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/',(req, res)=>{
+    res.send("connection is establish");
+})
+
+
+const uri = "mongodb+srv://mostofakamal:mk6683mk*@cluster0.zwqcs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const productCollection = client.db("carewarehouse").collection("products");
+//   // perform actions on the collection object
+//   console.log("mongodb Connection ok");
+ 
+// });
+
+
+const run = async()=>{
+
+
+  try{
+
+    await client.connect();
+
+    const database =  client.db('carewarehouse');
+
+    // get all products 
+    app.get('/allProducts', async(req, res)=>{
+
+      const productCollection =  database.collection('products');
+      const result = await productCollection.find({}).toArray();
+  
+      res.send(result);
+      console.log("result",result);
+
+    });
+
+
+    // add products
+
+    app.post('/add-products',async(req, res)=>{
+
+      // create object 
+
+      const data = req.body;
+
+     const result = await database.collection("products").insertOne(
+       {
+         productName:"abcd"
+       ,ProductDescription:'bbcdd', 
+       Price: 500
+       ,qty:20,
+        supplier:'aaaa'}
+       );
+
+
+      console.log("result ", result.insertedId);
+      
+
+      
+    });
+
+
+    // delete Products 
+
+    app.delete('/product/:id', async(req, res)=>{
+      const _id = req.params.id;
+      const query = {_id:objectId(_id)};
+      const result = await database.collection('products');
+      const rr = await result.deleteOne(query);
+      res.send(rr);
+
+
+    })
+ 
+    // update  products
+
+    app.put('/products/:id',(req, res)=>{
+
+      const id = req.params.id;
+      const products = req.body;
+      const updateColl = database.collection('products');
+      const filter = { _id: id };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          qty:products.qty,
+          Price: products.Price
+          
+        }
+        
+      };
+
+      const result = await updateColl.updateOne(filter, updateDoc, options);
+
+      res.send(result);
+
+      console.log(result);
+
+    });
+    
+
+  } finally{
+
+  }
+
+
+}
+
+
+
+run().catch(console.dir);
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log("Server is running Now");
+});
+
+
+
+
+
